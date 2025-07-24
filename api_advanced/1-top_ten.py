239927301f33e2e72537dade6e6a0b732a0083ba
GMwizera <g.mwizera@alustudent.com>
2025-07-24 12:22:14 +0200

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -18,20 +18,20 @@ def top_ten(subreddit):
         # If the subreddit does not exist or an error occurs, print 'None'
         if r.status_code != 200:
             import sys
-            sys.stdout.write("OK")
+            sys.stdout.write('None\n')
             return
 
         data = r.json().get('data', {}).get('children', [])
 
         if not data:
             import sys
-            sys.stdout.write("OK")
+            sys.stdout.write('None')
             return
 
         for post in data:
             title = post.get("data", {}).get("title", "")
             import sys
-            sys.stdout.write("OK")
+            sys.stdout.write(title + "\n")
     except Exception as e:
         import traceback
         traceback.print_exc()
