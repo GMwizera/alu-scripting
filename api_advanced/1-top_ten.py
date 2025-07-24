f2dc87e7c6a8bae8693bb6e2ac5cf0582f2e8862
GMwizera <g.mwizera@alustudent.com>
2025-07-24 12:08:43 +0200

    top-ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Function that queries the Reddit API"""
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    """Prints titles of first 10 hot posts for a given subreddit"""
+    """Prints titles of first 10 hot posts for a given subreddit using sys"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'linux:0:1.0 (by /u/JuiceExtension6952)'}
     params = {"limit": 10}
@@ -18,16 +19,17 @@ def top_ten(subreddit):
         )
 
         if response.status_code != 200:
-            print(None)
+            sys.stdout.write("None\n")
             return
 
         posts = response.json().get("data", {}).get("children", [])
 
         if not posts:
-            print(None)
+            sys.stdout.write("None\n")
             return
 
         for post in posts:
-            print(post.get("data", {}).get("title"))
+            title = post.get("data", {}).get("title", "")
+            sys.stdout.write(title + "\n")
     except Exception:
-        print(None)
+        sys.stdout.write("None\n")
