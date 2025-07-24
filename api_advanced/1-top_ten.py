66cf31e3961ed9b3745fd60d81b0653c5938afe5
GMwizera <g.mwizera@alustudent.com>
2025-07-24 12:13:01 +0200

    top-ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
 """Function that queries the Reddit API"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Prints titles of first 10 hot posts for a given subreddit using sys"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    """Prints titles of first 10 hot posts for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'linux:0:1.0 (by /u/JuiceExtension6952)'}
     params = {"limit": 10}
-
     try:
-        response = requests.get(
+        r = requests.get(
             url,
             headers=headers,
             params=params,
             allow_redirects=False
         )
-
-        if response.status_code != 200:
-            sys.stdout.write("None\n")
+        if r.status_code != 200:
+            import sys
+            sys.stdout.write('None\n')
             return
-
-        posts = response.json().get("data", {}).get("children", [])
-
-        if not posts:
-            sys.stdout.write("None\n")
+        
+        data = r.json().get('data', {}).get('children', [])
+        
+        if not data:
+            import sys
+            sys.stdout.write('None')
             return
-
-        for post in posts:
-            title = post.get("data", {}).get("title", "")
-            sys.stdout.write(title + "\n")
-    except Exception:
-        sys.stdout.write("None\n")
+        
+        for post in data:
+            import sys
+            sys.stdout.write(post.get('data', {}).get('title', ''))
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        # If an error occurs, print 'OK' to stdout
+        import traceback
+        traceback.print_exc()
