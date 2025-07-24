705abe23888b33cbe4ac3f676f582bf5362dc5be
GMwizera <g.mwizera@alustudent.com>
2025-07-24 12:06:30 +0200

    top-ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,18 +5,29 @@ import requests
 
 def top_ten(subreddit):
     """Prints titles of first 10 hot posts for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'linux:0:1.0 (by /u/JuiceExtension6952)'}
     params = {"limit": 10}
+
     try:
-        r = requests.get(
+        response = requests.get(
             url,
             headers=headers,
             params=params,
             allow_redirects=False
         )
-        import sys
-        sys.stdout.write('OK')
+
+        if response.status_code != 200:
+            print(None)
+            return
+
+        posts = response.json().get("data", {}).get("children", [])
+
+        if not posts:
+            print(None)
+            return
+
+        for post in posts:
+            print(post.get("data", {}).get("title"))
     except Exception:
-        import sys
-        sys.stdout.write('OK')
+        print(None)
